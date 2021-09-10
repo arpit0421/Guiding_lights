bdae73757e42a61af88dab240f26be0f14ea4d55
arpit0421 <arpit.j0421@gmail.com>
2021-09-10 23:05:52 +0530

    env modified

---
+++ pages/api/login.js
@@ -0,0 +1,35 @@
+import dbConnect from "../../helpers/dbconnect";
+import bcrypt from 'bcryptjs'
+import user from '../../models/user'
+import jwt from 'jsonwebtoken'
+
+
+dbConnect()
+
+export default async(req, res)=>{
+
+    const {email, password} = req.body
+    if(!email || !password){
+        res.status(422).json({error:"Input all fields"})
+        return
+    }
+
+    const User = await user.findOne({email: email})
+    if(!User){
+        res.status(422).json({error:"User not found"})
+        return
+    }
+
+    const matchPassword = await bcrypt.compare(User.password, password)
+    if(matchPassword){
+        const token = jwt.sign({userId: User._id}, process.env.JWT_SECRET, {
+            expiresIn:"2d"
+        })
+        res.status(201).json({token})
+    }
+    else{
+        res.status(422).json({error:"email or password don't match"})
+        return
+    }
+
+} 
--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,43 @@
+import { getCookieParser } from 'next/dist/server/api-utils'
 import Link from 'next/link'
 import {useState} from 'react'
+import cookie from 'js-cookie'
 
 const Login = ()=>{
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+
+    const userLogin = async(e)=>{
+
+        e.preventDefault()
+
+        const res = await fetch('http://localhost:3000/login', {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+
+        const res2 = res.json()
+        if(res2.error){
+            return res.status(422).json({error:"error occured while login"})
+        }
+        else{
+            console.log(res2)
+            cookie.set('token', res.token)
+               
+        }
+    }
     
     return(
         <div className="">
                 <div className="container w-full">
-                    <form className="flex-col flex mt-40">
+                    <form className="flex-col flex mt-40" onSubmit={(e)=>userLogin(e)}>
                         <h1>Login</h1>
                         <input type="email" value={email} name="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}}></input>
                         <input type="password" value ={password} name="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}}></input>
