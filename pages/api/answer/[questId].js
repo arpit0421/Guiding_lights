fd8017db4a522a64693d3bee61596693e48ea0e7
arpit0421 <arpit.j0421@gmail.com>
2021-09-29 15:28:56 +0530

    answers POST

--- pages/api/answer/[questId].js
+++ pages/api/answer/[questId].js
@@ -4,6 +4,18 @@ import question from "../../../models/question"
 dbConnect();
 
 export default async(req, res)=>{
+    switch(req.method)
+    {
+        case "GET":
+            getAnswer(reeq, res)
+            break
+        case "PUT":
+            postAnswer(req, res)
+            break
+    }
+}
+
+const getAnswer = async(req, res)=>{
     try {
         const {questid} = req.query
         const ques = await question.findOne({_id: questid})
@@ -15,4 +27,18 @@ export default async(req, res)=>{
         console.log(error)
     }
 }
-//hello
+
+const postAnswer = async(req, res)=>{
+    try {
+        const data = JSON.parse(req.body)
+        const ques = await question.findOne({_id: questid})
+        const ans = {name: data.name, answer: data.answer}
+        ques.answer.append(ans)
+        ques.save()
+        console.log(ques)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
